a7128be6ca7eba89bd27508fb4651fb29a453832
ehemdal <erik.hemdal@gmail.com>
2023-07-02 15:05:29 -0400

    Adding simple examples for gcov and gprof

---
+++ gc_demo/demo.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+  void f(void)
+  {
+	  printf("Inside function f\n");
+  }                    
+
+  void g(void)
+{
+	printf("\tInside function g\n");
+	sleep(1);
+}
+  int main(void)
+  {
+	for (int i=0; i < 999; i++) {
+     		f();
+		if (i % 100 == 0) {
+			g();
+		}
+	}
+     return 0;
+  }
